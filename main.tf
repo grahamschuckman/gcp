55a6c4232f525d6126cd51da54062cc8551950b7
Graham Schuckman <33704054+grahamschuckman@users.noreply.github.com>
2023-11-19 00:04:54 -0500

    Built a VPC in GCP.

--- main.tf
+++ main.tf
@@ -33,16 +33,18 @@ resource "google_project_service" "container" {
   service = "container.googleapis.com"
 }
 
-# # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
-# resource "google_compute_network" "main" {
-#   name                            = "main"
-#   routing_mode                    = "REGIONAL"
-#   auto_create_subnetworks         = false
-#   mtu                             = 1460
-#   delete_default_routes_on_create = false
+# Create a new VPC called main. Could use the existing default VPC, but this is a good exercise
+# Need to associate Compute Network Admin role with the service account to have permissions to create the VPC
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
+resource "google_compute_network" "main" {
+  name                            = "main"
+  routing_mode                    = "REGIONAL"
+  auto_create_subnetworks         = false
+  mtu                             = 1460
+  delete_default_routes_on_create = false
 
-#   depends_on = [
-#     google_project_service.compute,
-#     google_project_service.container
-#   ]
-# }
+  depends_on = [
+    google_project_service.compute,
+    google_project_service.container
+  ]
+}
