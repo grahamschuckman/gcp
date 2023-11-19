c57991aafb5694a9cffacf8f2c67e4606a28a621
Graham Schuckman <33704054+grahamschuckman@users.noreply.github.com>
2023-11-18 22:33:39 -0500

    Commit everything but the json keys used for creds.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # gcp
 Basic prototype of connecting to Google Cloud with Terraform.
+
+## Requirements
+
+| Name | Version |
+|------|---------|
+| google | 5.6.0 |
+
+## Providers
+
+No providers.
+
+## Modules
+
+No modules.
+
+## Resources
+
+No resources.
+
+## Inputs
+
+No inputs.
+
+## Outputs
+
+No outputs.
---
+++ main.tf
@@ -0,0 +1,19 @@
+# resource "random_id" "bucket_prefix" {
+#   byte_length = 8
+# }
+
+resource "google_storage_bucket" "default" {
+  name          = "schuckman-gcp-kubernetes-bucket-tfstate"
+  force_destroy = false
+  location      = "US"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = false
+  }
+  encryption {
+    default_kms_key_name = google_kms_crypto_key.terraform_state_bucket.id
+  }
+  depends_on = [
+    google_project_iam_member.default
+  ]
+}
